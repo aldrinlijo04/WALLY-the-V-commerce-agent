9f09afb2bb8a2fba23860a78d17b6d5134a00d61
Aldrin Lijo <aldrinlijo04@gmail.com>
2025-05-02 21:40:05 +0530

    comit form yesterday

--- tts.py
+++
@@ -1,9 +0,0 @@
-from gtts import gTTS
-import sys
-
-# Get the TTS response from command-line arguments
-tts_response = sys.argv[1]
-
-# Generate TTS audio
-tts = gTTS(tts_response)
-tts.save('tts_output.wav')
